309be3e11c2c229059fca87682815383d08b3041
cdeorta-dev <cdeorta.dev@gmail.com>
2025-07-02 14:51:23 -0300

    agregar

--- src/main.js
+++ src/main.js
@@ -7,13 +7,15 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDyVHdyeRnZvjku7P1AIKL9lV_b7D0E1z8",
-    authDomain: "hayfulbito-a200a.firebaseapp.com",
+
     projectId: "hayfulbito-a200a",
     storageBucket: "hayfulbito-a200a.firebasestorage.app",
     messagingSenderId: "138672255337",
     appId: "1:138672255337:web:f8dddc55e0048cc1638df8",
-    measurementId: "G-LY8KKR468W"
+    measurementId: "G-LY8KKR468W",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    // tus datos de config
 };
 
 const appPlic = initializeApp(firebaseConfig);
